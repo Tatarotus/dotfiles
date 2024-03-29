99c7c1c00c2cad7ade568e07ee297b9746440807
Samuel Baruch <shamuelbaruch@gmail.com>
2024-03-29 13:04:33 -0300

    add alias for exa

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias lsa="du -sh * | sort -hr"
+    alias ls="exa"
+    alias ll="exa -alh --group-directories-first"
+    alias tree="exa -T --git-ignore"
+    alias lsaa="exa --long --header --git -a --group-directories-first"
     alias vim="nvim"
     zoxide init fish | source
     alias cd="z $argv"
